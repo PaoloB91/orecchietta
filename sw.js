67d16535b86619ef5b8872d2ceafaafc39ed5ca7
PaoloB91 <paolo@pscompanysrl.com>
2026-08-18 11:03:14 +0200

    Aggiornamento app orecchietta 2026-08-18

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Service worker: rende l'app apribile anche senza rete.
    CACHE_VERSION va incrementata a ogni pubblicazione (lo fa deploy.sh). */
-const CACHE_VERSION = "orecchietta-v7";
+const CACHE_VERSION = "orecchietta-v8";
 const SHELL = [
   "./",
   "./index.html",
